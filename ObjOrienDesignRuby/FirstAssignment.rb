d0889bd420f7dc595cf95621351265adc6ec2b96
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-08-26 19:17:17 -0400

    tests for second problem

--- ObjOrienDesignRuby/FirstAssignment.rb
+++ ObjOrienDesignRuby/FirstAssignment.rb
@@ -11,13 +11,23 @@ def duple1(n, a)
   a.flat_map { |i| [i] * n }
 end
 
+# Problem 2
+def fib_array(i) end
+
 # test functions
 def problem1_tests
   array = [1, 2, 3]
-  puts "Problem 1: "
+  puts "Problem 1 - Array Duplication: "
   puts "Test 1: " + problem1_test_is_passed?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
   puts "Test 2: " + problem1_test_is_passed?(1, array, [1, 2, 3]).to_s
   puts "Test 3: " + problem1_test_is_passed?(0, array, []).to_s
+end
+
+def problem2_tests
+  puts "-------------------\n\nProblem 2 - Fibonacci Array:"
+  puts "Test 1: " + problem2_test_is_passed?(1, [1]).to_s
+  puts "Test 1: " + problem2_test_is_passed?(2, [1, 1]).to_s
+  puts "Test 1: " + problem2_test_is_passed?(1, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]).to_s
 end
 
 def problem1_test_is_passed?(n, array, expected)
@@ -25,5 +35,11 @@ def problem1_test_is_passed?(n, array, expected)
   actual == expected
 end
 
+def problem2_test_is_passed?(i, expected)
+  actual = fib_array(i)
+  actual == expected
+end
+
 # run tests
 problem1_tests
+problem2_tests
