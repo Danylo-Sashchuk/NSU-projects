8a80ce696cca9b78a4e5dadd4cb25034446d727b
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-08-26 19:04:09 -0400

    second approach for problem 1

--- ObjOrienDesignRuby/FirstAssignment.rb
+++ ObjOrienDesignRuby/FirstAssignment.rb
@@ -8,20 +8,19 @@ end
 
 # Using Array.flat_map method
 def duple1(n, a)
-  a.flat_map {|i| [i] * n}
+  a.flat_map { |i| [i] * n }
 end
-
 
 # test functions
 def problem1_tests
   array = [1, 2, 3]
   puts "Problem 1: "
-  puts "Test 1: " + problem1_test?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
-  puts "Test 2: " + problem1_test?(1, array, [1, 2, 3]).to_s
-  puts "Test 3: " + problem1_test?(0, array, []).to_s
+  puts "Test 1: " + problem1_test_is_passed?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
+  puts "Test 2: " + problem1_test_is_passed?(1, array, [1, 2, 3]).to_s
+  puts "Test 3: " + problem1_test_is_passed?(0, array, []).to_s
 end
 
-def problem1_test?(n, array, expected)
+def problem1_test_is_passed?(n, array, expected)
   actual = duple(n, array)
   actual == expected
 end
