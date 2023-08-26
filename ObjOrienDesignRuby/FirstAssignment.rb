46b545b4ad0afb8e3dfa22947689df5d19dd8f80
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-08-26 18:16:54 -0400

    second version in process

--- ObjOrienDesignRuby/FirstAssignment.rb
+++ ObjOrienDesignRuby/FirstAssignment.rb
@@ -1,42 +1,31 @@
 # Problem 1
-def duple(n, a)
+def duple1(n, a)
   result = Array.new
-  n.times do
-    a.each { |i| result << i }
-  end
+  a.each { |i| n.times { result << i } }
   result
 end
 
+def duple(n, a)
+  result = Array.new do
+    
+  end
+end
+
+#Problem 2
+
 # test functions
-def problem1_test1?
-  n = 4
-  a = [1, 2, 3]
-
-  expected = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
-  actual = duple(n, a)
-  expected == actual
+def problem1_tests
+  array = [1, 2, 3]
+  puts "Problem 1: "
+  puts "Test 1: " + problem1_test?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
+  puts "Test 2: " + problem1_test?(1, array, [1, 2, 3]).to_s
+  puts "Test 3: " + problem1_test?(0, array, []).to_s
 end
 
-def problem1_test2?
-  n = 1
-  a = [1, 2, 3]
-
-  expected = [1, 2, 3]
-  actual = duple(n, a)
-  expected == actual
-end
-
-def problem2_test3?
-  n = 0
-  a = [1, 2, 3]
-
-  expected = [1, 2, 3]
-  actual = duple(n, a)
-  expected == actual
+def problem1_test?(n, array, expected)
+  actual = duple(n, array)
+  actual == expected
 end
 
 # run tests
-puts "Problem 1: "
-puts "Test 1: " + problem1_test1?.to_s
-puts "Test 2: " + problem2_test3?.to_s
-puts "Test 3: " + problem2_test3?.to_s
+problem1_tests
