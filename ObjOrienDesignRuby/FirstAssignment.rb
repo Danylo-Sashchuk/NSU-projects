08d28f76ac8f55aa6c2aef7c3e01d28be41ac283
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-08-27 19:02:02 -0400

    is_palindrome tests

--- ObjOrienDesignRuby/FirstAssignment.rb
+++ ObjOrienDesignRuby/FirstAssignment.rb
@@ -32,42 +32,57 @@ def fib(n)
   second
 end
 
+# Problem 4
+def is_palindrome?(s) end
+
 # test functions
 def problem1_tests
   array = [1, 2, 3]
   puts "Problem 1 - Array Duplication: "
-  puts "Test 1: " + problem1_test_is_passed?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
-  puts "Test 2: " + problem1_test_is_passed?(1, array, [1, 2, 3]).to_s
-  puts "Test 3: " + problem1_test_is_passed?(0, array, []).to_s
+  puts "Test 1: " + test_problem1?(4, array, [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]).to_s
+  puts "Test 2: " + test_problem1?(1, array, [1, 2, 3]).to_s
+  puts "Test 3: " + test_problem1?(0, array, []).to_s
 end
 
 def problem2_tests
   puts "-------------------\n\nProblem 2 - Fibonacci Array:"
-  puts "Test 1: " + problem2_test_is_passed?(1, [1]).to_s
-  puts "Test 2: " + problem2_test_is_passed?(2, [1, 1]).to_s
-  puts "Test 3: " + problem2_test_is_passed?(10, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]).to_s
+  puts "Test 1: " + test_problem2?(1, [1]).to_s
+  puts "Test 2: " + test_problem2?(2, [1, 1]).to_s
+  puts "Test 3: " + test_problem2?(10, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]).to_s
 end
 
 def problem3_tests
   puts "-------------------\n\nProblem 3 - Fibonacci Number:"
-  puts "Test 1: " + problem3_test_is_passed?(10, 55).to_s
-  puts "Test 2: " + problem3_test_is_passed?(100, 354224848179261915075).to_s
-  puts "Test 3: " + problem3_test_is_passed?(1, 1).to_s
-  puts "Test 4: " + problem3_test_is_passed?(2, 1).to_s
+  puts "Test 1: " + test_problem3?(10, 55).to_s
+  puts "Test 2: " + test_problem3?(100, 354224848179261915075).to_s
+  puts "Test 3: " + test_problem3?(1, 1).to_s
+  puts "Test 4: " + test_problem3?(2, 1).to_s
 end
 
-def problem1_test_is_passed?(n, array, expected)
+def problem4_tests
+  puts "-------------------\n\nProblem 4 - Is a Palindrome:"
+  puts "Test 1: " + test_problem4?("abba", true).to_s
+  puts "Test 2: " + test_problem4?("a", true).to_s
+  puts "Test 3: " + test_problem4?("peach", false).to_s
+end
+
+def test_problem1?(n, array, expected)
   actual = duple(n, array)
   actual == expected
 end
 
-def problem2_test_is_passed?(i, expected)
+def test_problem2?(i, expected)
   actual = fib_array(i)
   actual == expected
 end
 
-def problem3_test_is_passed?(i, expected)
+def test_problem3?(i, expected)
   actual = fib(i)
+  actual == expected
+end
+
+def test_problem4?(s, expected)
+  actual = is_palindrome?(s)
   actual == expected
 end
 
@@ -75,3 +90,4 @@ end
 problem1_tests
 problem2_tests
 problem3_tests
+problem4_tests
