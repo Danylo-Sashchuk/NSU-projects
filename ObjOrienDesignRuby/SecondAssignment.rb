928647cd3bb02ff1c75f866fc5ee501b3db36c9e
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-09-07 22:34:20 -0400

    waiting for refactoring

--- ObjOrienDesignRuby/SecondAssignment.rb
+++ ObjOrienDesignRuby/SecondAssignment.rb
@@ -52,12 +52,13 @@ class DNA
   end
 end
 
+# Class with static methods for convenient testing
 class TestEngine
   RESET = "\e[0m"
   FAIL = "\e[31m"
   PASS = "\e[32m"
 
-  def testing(expected)
+  def self.testing(expected)
     actual = yield
     if actual == expected
       "#{PASS}Passed #{RESET}"
@@ -66,71 +67,63 @@ class TestEngine
     end
   end
 
-  def run_tests(clazz)
-    methods = clazz.methods.select { |method_name| method_name.to_s.match(/^test\d+/) }.sort
+  # Method for searching and running test methods in a class
+  def self.run_tests(clazz)
+    methods = clazz.methods.select { |method_name| method_name.to_s.match(/^test\d+/) }
+    methods = methods.sort_by { |method_name| method_name.to_s[/\d+/].to_i }
     methods.each { |method| clazz.send(method) }
   end
 end
 
 class DNA_Test
-  attr_reader :test_engine
-
-  def initialize
-    @test_engine = TestEngine.new
-  end
-
-  def run_tests
-    puts "-------------------\nDNA Tests:"
-    @test_engine.run_tests(self)
-  end
 
   def test1
     dna = DNA.new("ATTGCC")
-    puts "Test 1: " + @test_engine.testing(6) { dna.length }
+    puts "Test 1: " + TestEngine.testing(6) { dna.length }
   end
 
   def test2
     dna = DNA.new("ATTGCC")
-    puts "Test 2: " + @test_engine.testing("ATTGCC") { dna.to_s }
+    puts "Test 2: " + TestEngine.testing("ATTGCC") { dna.to_s }
   end
 
   def test3
     dna = DNA.new("ATTGCC")
     another_dna = DNA.new("TGC")
-    puts "Test 3: " + @test_engine.testing(true) { dna.contains?(another_dna) }
+    puts "Test 3: " + TestEngine.testing(true) { dna.contains?(another_dna) }
   end
 
   def test4
     dna = DNA.new("ATTGCC")
     another_dna = DNA.new("AT")
-    puts "Test 4: " + @test_engine.testing(true) { dna.contains?(another_dna) }
+    puts "Test 4: " + TestEngine.testing(true) { dna.contains?(another_dna) }
   end
 
   def test5
     dna = DNA.new("ATTGCC")
     another_dna = "GG"
-    puts "Test 5: " + @test_engine.testing(false) { dna.contains?(another_dna) }
+    puts "Test 5: " + TestEngine.testing(false) { dna.contains?(another_dna) }
   end
 
   def test6
     dna = DNA.new("ATTGCC")
-    puts "Test 6: " + @test_engine.testing([1, 2]) { dna.positions("T") }
+    puts "Test 6: " + TestEngine.testing([1, 2]) { dna.positions("T") }
   end
 
   def test7
     dna1 = DNA.new("ATTGCC")
     dna2 = DNA.new("GTTGAC")
-    puts "Test 7: " + @test_engine.testing(2) { dna1.hamming_distance(dna2) }
+    puts "Test 7: " + TestEngine.testing(2) { dna1.hamming_distance(dna2) }
   end
 
   def test8
     dna1 = DNA.new("ATTGCC")
-    puts "Test 8: " + @test_engine.testing(0) { dna1.hamming_distance(dna1) }
+    puts "Test 8: " + TestEngine.testing(0) { dna1.hamming_distance(dna1) }
   end
 
   def test9
     dna = DNA.new("ATTGCC")
-    puts "Test 9: " + @test_engine.testing(nil) {
+    puts "Test 9: " + TestEngine.testing(nil) {
       begin
         dna.hamming_distance(DNA.new('AT'))
       rescue ArgumentError => e
@@ -143,9 +136,8 @@ class DNA_Test
   def test10
     dna = DNA.new("ATTGCC")
     expected = { "A" => 1, "T" => 2, "G" => 1, "C" => 2 }
-    puts "Test 10: " + @test_engine.testing(expected) { dna.frequencies }
+    puts "Test 10: " + TestEngine.testing(expected) { dna.frequencies }
   end
 end
 
-dna_tests = DNA_Test.new
-dna_tests.run_tests
+TestEngine.run_tests(DNA_Test.new)
