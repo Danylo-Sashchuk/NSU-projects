5d2eea78b7eaa1a934dd1e674d61c7a9d3f60f3d
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-09-26 19:14:51 -0400

    strategies introduced

--- ObjOrienDesignRuby/third_assignment.rb
+++ ObjOrienDesignRuby/third_assignment.rb
@@ -2,15 +2,40 @@
 
 require 'colorize'
 
+class Printer
+  def output(word)
+    print("#{word}\n")
+  end
+end
+
+class LinePrinter < Printer
+  def output(word) end
+end
+
+class ColorizePrinter < Printer
+  def output(word) end
+end
+
 class Wordly
-  attr_reader :word
+  attr_reader :word, :printer
+  attr_accessor :color
+
+  def color=(value)
+    if value == true
+      @printer = ColorizePrinter.new
+    elsif value == false
+      @printer = LinePrinter.new
+    else
+      print("Wrong parameter! Values are limited by 'true' and 'false'. \n")
+    end
+  end
 
   def choose_word
     'abort'
   end
 
   # TODO: add comments
-  def transform_word(word)
+  def parse_word(word)
     hash = {}
     word.chars.each_with_index do |letter, index|
       hash[letter] ||= [] # Initialize an empty array if the letter is not yet in the hash
@@ -20,11 +45,11 @@ class Wordly
   end
 
   def initialize
-    raw_word = choose_word
-    @word = transform_word(raw_word)
+    target_word = choose_word
+    @word = parse_word(target_word)
   end
 
-  def check_input(input)
+  def check_word(input)
     @word.chars.each_with_index do |char, index|
 
     end
@@ -32,13 +57,14 @@ class Wordly
 
   def play
     print "Guess the five-letter word:\n"
-    loop do
-      input = user_input
-      result = check_input(input)
-      print result
+    6.times do
+      word = user_input
+      result = check_word(word)
+      @printer.output(result)
+      break if correct?(result)
     end
   end
 end
 
 w = Wordly.new
-print w.transform_word 'error'
+print w.parse_word 'error'
